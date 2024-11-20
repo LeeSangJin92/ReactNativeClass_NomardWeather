98a0139c635bc41bb837ecf149a37a94baf9916c
LeeSangJin92 <151722889+LeeSangJin92@users.noreply.github.com>
2024-11-20 16:11:55 +0900

    2024. 11. 20 ✏️ 리액트 네이티브 스타일 부여 방법
    ✏️ 총 3가지의 방법 구현
    
    📕 첫번쨰 : 태그의 style 속성에 직접적으로 스타일 코드 입력
    ⚠️ 간단한 코드 시 빠르게 코드 입력이 가능하지만 추천하는 방법은 아님
    
    📗 두번째 : 스타일 코드가 입력된 객체 타입의 컴포넌트를 style 속성에 부여
    ⚠️ 스타일 코드로 생성된 컴포넌트를 style 속성에 부여하여 스타일 지정
    
    📘 3번째 방법 : StyleSheet.create를 활용하여 스타일 컴포넌트 생성하여 style 속성에 부여
    ⭐ 2번째 방법과 동일하지만 StyleSheet.create에서 스타일 코드 관련 내용을 자동완성하기 때문에 매우 유용함! ⭐⭐⭐

--- App.js
+++ App.js
@@ -4,12 +4,33 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Go on Expo@</Text>
+      <Text style={styles.text}>스타일 지정하는 방법</Text>
+      
+      {/* 1) style 속성에 직접적으로 스타일 코드 입력하는 방법 */}
+      <Text style={{
+        fontSize:30,
+        color:"red",
+      }}>- 📕 첫번째 -</Text>
+      
+      {/* 2) 스타일 코드가 입려된 컴포넌트 호출하는 방법 */}
+      <Text style={styleCode.text}>- 📗 두번째 -</Text>
+
+      {/* 3) 2번째 방법과 동일하지만 StyleSheet.create를 활용한 방법 */}
+      <Text style={styles.text}>- 📘 세번째 -</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+// 📗 2번째 방법의 스타일 코드
+const styleCode = {
+  text:{
+    fontSize:30,
+    color:"green"
+  }
+};
+
+// 📘 3번쨰 방법의 스타일 코드
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -17,4 +38,13 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+    fontSize:30,
+  }
 });
+
+/* 🔍2번째와 3번째 방법의 차이점 
+  2번째와 3번째 차이점은 3번째 방법은 스타일 코드를
+  입력 시 자동완성 기능이 있어서 코드 입력 시 효율적으로
+  스타일 코드를 작성할 수 있음 
+*/
