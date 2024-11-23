d3bdd8984ceb12d5fc1b109b750767ba68ead05c
LeeSangJin92 <151722889+LeeSangJin92@users.noreply.github.com>
2024-11-23 19:20:29 +0900

    2024. 11. 23 ✅ ScrollView / Dimensions
    ✅ ScrollView Components 활용 및 Prop 기능 학습
    ✏️ 앱에서 스크롤 기능 적용하는 컴포넌트
    📘 contentContainerStyle Prop
    📘 pagingEnabled Prop
    📘 showsHorizontalScrollIndicator Prop
    📘 showsVerticalScrollIndicator Prop
    
    ✅ Dimensions API 활용법 학습
    ✏️ 현재 사용 중인 widow의 넓이와 높이 크기를 가져옴

--- App.js
+++ App.js
@@ -1,43 +1,67 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, ScrollView, StyleSheet, Text, View } from 'react-native';
+
+const {width:SCREEN_WIDTH} = Dimensions.get("window");
 
 export default function App() {
+
   return (
     <View style={styles.continue}>
       <View style={styles.city}>
         <Text style={styles.cityName}>Seoul</Text>
       </View>
-      <View style={styles.weather}>
+      <ScrollView 
+      horizontal 
+      pagingEnabled 
+      showsHorizontalScrollIndicator={false}
+      contentContainerStyle={styles.weather}>
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.description}>Sunny</Text>
         </View>
-      </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  // 앱 컨터이너
   continue:{
     flex:1,
     backgroundColor:"tomato"
   },
+
+  // 날씨 체크 기준이 되는 도시 박스 Layout
   city:{
     flex:1,
     justifyContent:"center",
     alignItems:"center"
   },
+
+  // 날씨 체크 기준이 되는 도시 이름 Text
   cityName:{
     fontSize:68,
     fontWeight:500
   },
 
+  // 날씨가 표시되는 Layout ( ScrollView )
   weather:{
-    flex:3,
   },
 
   day:{
-    flex:1,
+    width:SCREEN_WIDTH,
     alignItems:"center"
   },
 
